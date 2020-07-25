00257355c5e509a2cea78eb889a593cae4c69173
alexander017 <68747402+alexander017@users.noreply.github.com>
2020-07-25 13:26:01 +0200

    Update build_windows.sh

--- build_windows.sh
+++ build_windows.sh
@@ -1,6 +1,6 @@
 export PATH=/cygdrive/c/tools/cygwin/bin:$PATH
 
-wget -q -O OpenJDK11_x64_Windows.zip "https://api.adoptopenjdk.net/v2/binary/releases/openjdk11?os=windows&release=jdk-11.0.3%2B7&arch=x64&type=jdk"
+wget -q -O OpenJDK11_x64_Windows.zip "https://api.adoptopenjdk.net/v2/binary/releases/openjdk11?os=windows&release=jdk-11.0.8%2B10&arch=x64&type=jdk&openjdk_impl=hotspot"
 JDK_BOOT_DIR=$PWD/$(unzip -Z1 OpenJDK11_x64_Windows.zip | grep 'bin/javac'  | tr '/' '\n' | tail -3 | head -1)
 unzip -q OpenJDK11_x64_Windows.zip
 
@@ -11,4 +11,4 @@ export JAVA_HOME=${JDK_BOOT_DIR}
 
 cd ./openjdk-build
 export LOG=info
-./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm  --branch dcevm11-jdk-11.0.7-adopt --jdk-boot-dir ${JDK_BOOT_DIR} --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --disable-test-image --check-fingerprint false --configure-args '-disable-warnings-as-errors --disable-hotspot-gtest' --target-file-name java11-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk11u
+./makejdk-any-platform.sh --tag "${SOURCE_JDK_TAG}" --build-variant dcevm  --branch asdcevm11-jdk-11.0.8-adopt --jdk-boot-dir ${JDK_BOOT_DIR} --hswap-agent-download-url ${HSWAP_AGENT_DOWNLOAD_URL} --disable-test-image --check-fingerprint false --configure-args '-disable-warnings-as-errors --disable-hotspot-gtest' --target-file-name java11-openjdk-dcevm-${TRAVIS_OS_NAME}.zip jdk11u
